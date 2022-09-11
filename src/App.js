57c1e675a1624fafc713f4e1d909ac448d5e184e
samuel2002Es <samuel2002espinoza@gmail.com>
2022-09-10 21:21:19 -0500

    subida

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="login-form">
+        <div className="title">Terminos y condiciones</div>
+        <div className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat doloremque ipsam magni nobis iusto, mollitia, obcaecati officia dolor laboriosam numquam facilis modi blanditiis. Ipsa optio magni culpa at ea nemo!
+    Facere omnis voluptas itaque, dignissimos similique excepturi quibusdam enim molestias error dolorem incidunt eos ex praesentium dolor, consequatur libero voluptatibus obcaecati. Corrupti fuga neque dignissimos voluptatum harum quis, soluta consequuntur.
+    Ut incidunt harum voluptatibus vitae deleniti. Odio repellendus dignissimos totam deserunt veniam, est sequi et deleniti fugit cum eveniet quibusdam quidem incidunt obcaecati laudantium, natus eius dolor voluptatem? Ex, necessitatibus!
+    Sed a accusantium recusandae, sint ut exercitationem explicabo, eaque nisi, harum nihil tempore. Cum quis ab, illum natus doloremque odit? Laboriosam ea, adipisci qui consequatur omnis corporis voluptatum repellendus aspernatur.
+    Eos aspernatur beatae numquam nostrum. Ab, non. Nobis laboriosam libero alias sit accusantium beatae reprehenderit illo numquam molestias similique iusto magnam odit quis deleniti veritatis dolor, in nesciunt sint. Iste.
+        </div>
+        <input className='button' type='button' value="Aceptar"></input>
+      </div>
     </div>
   );
 }
