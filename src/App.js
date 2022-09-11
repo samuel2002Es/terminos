c070c722d7649b9deb53563febbdf45c4d341b03
samuel2002Es <samuel2002espinoza@gmail.com>
2022-09-11 15:26:37 -0500

    terminos

--- src/App.js
+++ src/App.js
@@ -7,11 +7,8 @@ function App() {
       <div className="login-form">
         <div className="title">Terminos y condiciones</div>
         <div className="text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat doloremque ipsam magni nobis iusto, mollitia, obcaecati officia dolor laboriosam numquam facilis modi blanditiis. Ipsa optio magni culpa at ea nemo!
-    Facere omnis voluptas itaque, dignissimos similique excepturi quibusdam enim molestias error dolorem incidunt eos ex praesentium dolor, consequatur libero voluptatibus obcaecati. Corrupti fuga neque dignissimos voluptatum harum quis, soluta consequuntur.
-    Ut incidunt harum voluptatibus vitae deleniti. Odio repellendus dignissimos totam deserunt veniam, est sequi et deleniti fugit cum eveniet quibusdam quidem incidunt obcaecati laudantium, natus eius dolor voluptatem? Ex, necessitatibus!
-    Sed a accusantium recusandae, sint ut exercitationem explicabo, eaque nisi, harum nihil tempore. Cum quis ab, illum natus doloremque odit? Laboriosam ea, adipisci qui consequatur omnis corporis voluptatum repellendus aspernatur.
-    Eos aspernatur beatae numquam nostrum. Ab, non. Nobis laboriosam libero alias sit accusantium beatae reprehenderit illo numquam molestias similique iusto magnam odit quis deleniti veritatis dolor, in nesciunt sint. Iste.
+        Esta página web es propiedad y está operado por VEXTA. Estos Términos establecen los términos y condiciones bajo los cuales tu puedes usar nuestra página web y servicios ofrecidos por nosotros. Esta página web ofrece a los visitantes un registro seguro ademas de confiabilidad en su uso. Al acceder o usar la página web de nuestro servicio, usted aprueba que haya leído, entendido y aceptado estar sujeto a estos Términos:
+
         </div>
         <div className='bottonperro'>
           <a className='button' href="https://escoger.herokuapp.com/">Aceptar</a>
